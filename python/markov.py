53c0f811e79ca6a5b2ca7833ee2741915f157f61
yearn2learn <yearn2learn@users.noreply.github.com>
2016-06-14 14:48:04 -0400

    Markov Chain Core Code
    Capitalizes first letter of string and continues till finds a last word with a period.

--- python/markov.py
+++ python/markov.py
@@ -12,4 +12,47 @@
 
 # There are design choices to make; feel free to experiment and shape the program as you see fit. Jeff Atwood's [Markov and You](http://blog.codinghorror.com/markov-and-you/) is a fun place to get started learning about what you're trying to make.
 
-> REPLACE THIS TEXT WITH YOUR PROGRAM
+import random
+import sys
+
+class Markov(object):
+	
+	def __init__(self, file, size):
+		self.size = int(size)
+		self.words = self.get_words(file)
+		self.word_chain = self.database()
+		self.output = self.gen_text()
+
+	def get_words(self, data):
+		with open(data, 'rb') as f:
+			return f.read().split()
+
+	def database(self):
+		word_dict = {}
+		for x in range(len(self.words) - 2):
+			word_dict.setdefault( (self.words[x], self.words[x+1]) , []).append(self.words[x + 2])	
+		return word_dict
+
+	def gen_text(self):
+		starter = random.randint(0, len(self.words) - 2)
+		w1, w2 = self.words[starter], self.words[starter+1]
+		new_text = []
+		for x in range(self.size-1):
+			new_text.append(w1)
+			w1, w2 = w2, random.choice( self.word_chain[(w1, w2)] )
+		new_text.append(w2)
+		needs_period = True
+		while needs_period:
+			try:
+				w1, w2 = w2, random.choice( filter( lambda x: x[-1]== "." ,self.word_chain[(w1, w2)] ) )
+				needs_period = False
+			except IndexError:
+				w1, w2 = w2, random.choice( self.word_chain[(w1, w2)] )
+		new_text.append(w2)
+		text = " ".join(new_text)
+		text = text[0].upper() + text[1:]
+		print text
+		return text
+
+if __name__ == "__main__":
+	Markov(sys.argv[1], sys.argv[2])
